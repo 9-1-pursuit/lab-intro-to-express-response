c21a7d72ac9135a3a10fdc4a6a53e076560197f6
Gillesedgy <edgygilles@pursuit.org>
2022-12-13 17:17:52 -0500

    Did Bonus

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 // const CSS = require("/app.css");
-const PORT = process.env.PORT || 3003;
-const str = "Hello there!\nTry one of these routes.";
+// const PORT = process.env.PORT || 3003;
+const str = "Hello World!";
 //
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(418).send("Hello, world!");
+});
+
 //* bring in magic8ball file
 // const magic8ball = require("./Magic8ball");
 // const randomResponse = require("randomResponse");
 // let random_index = require("random_index");
-//
-app.get("/", (req, res) => {
-  res.send(str);
+
+//Terminator
+app.get("/terminator", (req, res) => {
+  res.send("Hasta la vista, baby");
+  res.send("Ill be back");
+  // only logs the first message --> overwrites the second message
 });
+
 //Tim Gunn
 app.get("/tim-gunn", (req, res) => {
   res.send("Make it work");
@@ -37,28 +46,24 @@ app.get("/zeus", (req, res) => {
   res.send("Release the Kraken");
 });
 
-//James Bond -
+//James Bond
 app.get("/jamesbond", (req, res) => {
   res.send("the name is Bond, James Bond");
 });
-//Rod Tidwell -
+
+//Rod Tidwell
 app.get("/rod-tidwell", (req, res) => {
   res.send("Show me the money!");
 });
-//Rocky -
+
+//Rocky
 app.get("/rocky", (req, res) => {
   res.send("Yo, Adrian");
 });
-//Jack Dawson -
 
+//Jack Dawson
 app.get("/jackdawson", (req, res) => {
   res.send(`<h1>I'm king of the world!</h1>`);
-});
-//Terminator
-app.get("/terminator", (req, res) => {
-  res.send("Hasta la vista, baby");
-  res.send("Ill be back");
-  // only logs the first message
 });
 
 //! Magic8Ball
@@ -84,18 +89,18 @@ const magic8Responses = [
   "Very doubtful",
 ];
 
-//randomly returns a response
-
-// const handleRandomResponse = () => {
 const random_index = Math.floor(Math.random() * magic8Responses.length);
 
 let randomResponse = magic8Responses[random_index];
-// };
-// handleRandomResponse();
 
 app.get("/magic8ball", (req, res) => {
-  res.send(randomResponse);
+  res.send(
+    `<h1 style ="color:blue; font-family: Gill Sans, sans-serif" >${randomResponse}</h1>`
+  );
 });
-app.listen(PORT, () => {
-  console.log(`Very active on ${PORT}`);
-});
+
+// app.listen(PORT, () => {
+//   console.log(`Very active on ${PORT}`);
+// });
+
+module.exports = app;
