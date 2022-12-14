600e3b3f0ba7d84a96a60758fc58ccbe4cac1a0d
bowersjuan <bowersjuan@gmail.com>
2022-12-14 03:49:42 -0500

    Update app.js and create catchPhrases.json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.argv.PORT || 3003;
 // This logic allows for either a defined port in .env and the deployed site or the assigned 3003 port
 
 const movies = require("./movies.json");
+const quotes = require("./catchPhrases.json");
 
 app.get("/", (req, res) => {
   res.send("<h2>Movies App running...</h2>");
@@ -17,7 +18,14 @@ app.get("/movies", (req, res) => {
 
 app.get("/movies/:id", (req, res) => {
   const { id } = req.params;
-  res.send(movies.find((movie) => movie.movieId === id));
+  const movie = movies.find((movie) => movie.movieId === id);
+  res.send(movie);
+});
+
+app.get("/:name", (req, res) => {
+  const { name } = req.params;
+  const quote = quotes.find((quote) => quote.quotee === name.toLowerCase());
+  res.send(`<h1>${quote?.quote || "404: Page Not Found"}</h1>`);
 });
 
 app.listen(PORT, (err) => {
