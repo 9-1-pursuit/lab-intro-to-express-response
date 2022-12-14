7fc223dd142fc219632cb4e3e5ad561bac38e2c9
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-14 00:30:56 -0500

    magic8 added

--- app.js
+++ app.js
@@ -5,32 +5,43 @@ const app = express()
 // set up listener for app 
 app.listen(3003)
 // get data from data.js file
-const {punchlines, homePage} = require("./data.js")
+const {punchlines, homePage, reference, objLoop, magic8Responses} = require("./data.js")
 
 //  create route for landing page
 app.get("/", (req, resp) => {
     resp.send(homePage)
 })
-
+// ACTIVITY MAGIC 8 BALL
+// need to put BEFORE :value endpoint otherwise will think magic8 is param
+app.get("/magic8", (req,resp) => {
+    resp.send(`
+    <h1>${magic8Responses[Math.floor(Math.random() * magic8Responses.length)]}</h1>
+    <br>
+    <a href="http://localhost:3003/">Home</a>
+    `)
+})
 
 // test params on root path
-// .params -> (middleware runs b4 the .get, or .put etc) to look into data objects and set key value to be used in app.get .send
+// .params -> (middleware runs b4 the .get, or .put etc) to look into data objects and set key value to be used in app.get resp.send
 app.param("value", (req, resp, next, value) => {
     const input = value.split(` `).join(``).toLowerCase()
     let punchline;
-    for (let key in punchlines){
-        if (input === key.toLowerCase()){
-            punchline = punchlines[key] 
-        }
-    }
-    req.punchline = punchline
+    let ref;
+    req.punchline = objLoop(punchline, punchlines, input)
+    req.reference = objLoop(ref, reference, input)
     next()
 })
 
 app.get("/:value", (req,resp) => {
-    // req.punchline is available here from 'middleware' .params function
-    resp.send(`${req.punchline ? req.punchline : "Error, Page Not Found"}`)
+    // req.punchline/ req.reference is available here from 'middleware' .params function
+    resp.send(`
+    ${req.punchline ? `<h2>${req.punchline} - (${req.reference})</h2>` : "Error, Page Not Found"}
+    <br>
+    <a href="http://localhost:3003/">Home</a>
+    `)
 }) 
+
+
 
 
 
--- data.js
+++ data.js
@@ -16,7 +16,7 @@ const reference = {
     DaenerysTargaryen: "Game of Thrones", 
     BoJackHorseman : "BoJack Horseman",
     Regis: "Who Wants To Be A Millionaire?",
-    TonyMontana: "ScarFace",
+    TonyMontana: "Scarface",
     Gollum: "Lord of the Rings",
     Hannibal: "Silence of the Lambs",
     Goofy: "A Goofy Movie"
@@ -24,10 +24,46 @@ const reference = {
 
 const homePage = `
 <h1>Home Page</h1>
-<p>Possible endpoints : Fraiser, Stan Marsh, Daenerys Targaryen, Bojack Horseman, Regis, Tony Montana, Gollum, Hannibal, Goofy </p>`
+<p>Possible endpoints : Fraiser, Stan Marsh, Daenerys Targaryen, Bojack Horseman, Regis, Tony Montana, Gollum, Hannibal, Goofy, magic8 </p>`
+
+// function for looping into data objects
+function objLoop(variable, obj, input){
+    for (let key in obj){
+        if(input === key.toLowerCase()){
+            variable = obj[key]
+        }
+    }
+    return variable
+}
+
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  
+  ]
 
 module.exports = {
     punchlines,
     reference,
-    homePage
+    homePage,
+    objLoop,
+    magic8Responses
 }
