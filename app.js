34f19f51192b7a8a3228ceb18ae3f56a324158c7
bowersjuan <bowersjuan@gmail.com>
2022-12-14 02:32:19 -0500

    Update app.js routes and create movies.json data

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.argv.PORT || 3003;
+// Ports are also handled by deployment environmental variables
+// This logic allows for either a defined port in .env and the deployed site or the assigned 3003 port
+
+const movies = require("./movies.json");
 
 app.get("/", (req, res) => {
-  res.send("App running");
+  res.send("<h2>App running...</h2>");
 });
 
-app.get("/terminator", (req, res) => {
-  res.send("I'll be back");
-  res.send("Hasta la vista baby!");
+app.get("/movies", (req, res) => {
+  res.send(movies);
+});
+
+app.get("/movies/terminatorjson", (req, res) => {
+  res.send();
 });
 
 app.get("/tim-gunn", (req, res) => {
