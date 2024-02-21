11b3c4fe4f5804b350c860f975dad5c13a0241fa
MarlonPelau <MarlonRegis@pursuit.org>
2024-02-21 00:41:57 -0500

    a host of dif requests and responses

--- app.js
+++ app.js
@@ -5,9 +5,47 @@ const app = express();
 const PORT = 3004;
 
 
-app.get('/bob-marley', (request, response) => {
-    response.send("some people are so poor, all they have is money")
+app.get('/terminator', (request, response) => {
+    response.send("I'll be back")
+    response.send("Hasta la vista, baby")
   });
+
+app.get('/bob-marley', (request, response) => {
+    response.send("money make ya rich?")
+});
+
+app.get('/homer-simpson', (request, response) => {
+    response.send("D'OH")
+});
+
+app.get('/50-cent', (request, response) => {
+    response.send("get rich or die trying")
+});
+
+app.get('/stevie-wonder', (request, response) => {
+    response.send("isn't she lovely, isn't she wonderful!")
+});
+
+app.get('/muhammad-ali', (request, response) => {
+    response.send("float like a butterfly, sting like a bee!")
+});
+
+app.get('/MLK', (request, response) => {
+    response.send("I have a dream!")
+});
+
+app.get('/regis', (request, response) => {
+    response.send("Is that your final answer?")
+});
+
+app.get('/shaggy', (request, response) => {
+    response.send("mr. lover lover")
+});
+
+app.get('/marlon-regis', (request, response) => {
+    response.send("variety is the spice of life..")
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
