8bf46dff696d3c3af3cd93a2b4f5421dc26b7b1f
MarlonPelau <MarlonRegis@pursuit.org>
2024-02-21 01:27:28 -0500

    got two dif random responses from magic8 request

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    // the first one to appear randomly in URL
+    "You may rely on it", 
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    // the second one to appear randomly in URL
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  
+  ]
 
 const PORT = 3004;
 
@@ -8,6 +32,7 @@ const PORT = 3004;
 app.get('/terminator', (request, response) => {
     response.send("I'll be back")
     response.send("Hasta la vista, baby")
+    // I saw the first message not the second
   });
 
 app.get('/bob-marley', (request, response) => {
@@ -46,6 +71,12 @@ app.get('/marlon-regis', (request, response) => {
     response.send("variety is the spice of life..")
 });
 
+app.get('/magic8', (request, response) => {
+    const randomIndex = Math.floor(Math.random() * magic8Responses.length);
+    const randomResponse = magic8Responses[randomIndex];
+
+    response.send(randomResponse);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
